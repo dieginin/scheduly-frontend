87cdcb59dcf4ccb87f708e05ee2f5597b3144574
dieginin <diegobalestra9@gmail.com>
2026-03-16 23:06:53 -0700

    fix: update CurrentShift to use shift data for lunch and worked time calculations

--- src/app/pages/home/ui/CurrentShift.tsx
+++ src/app/pages/home/ui/CurrentShift.tsx
@@ -1,12 +1,13 @@
 import { Card, CardContent, CardTitle } from "@/shared/components/ui/card"
 import { Coffee, Toolbox } from "lucide-react"
+import { lunchDuration, shiftDuration } from "@/app/lib/report.utils"
 
 import { StatusBadge } from "./StatusBadge"
 import { TimeBadge } from "./TimeBadge"
 import { useReport } from "@/app/hooks/useReport"
 
 export const CurrentShift = () => {
-  const { status, lunchTime, workedTime } = useReport()
+  const { shift, status } = useReport()
 
   return (
     <Card className='text-xl font-thin tracking-widest text-center lg:hidden'>
@@ -14,8 +15,8 @@ export const CurrentShift = () => {
       <CardContent className='flex justify-around'>
         <StatusBadge status={status} />
         <div className='flex gap-4 text-sm'>
-          <TimeBadge icon={Coffee} time={lunchTime} color='idle' />
-          <TimeBadge icon={Toolbox} time={workedTime} color='primary' />
+          <TimeBadge icon={Coffee} time={lunchDuration(shift)} color='idle' />
+          <TimeBadge icon={Toolbox} time={shiftDuration(shift)} color='primary' />
         </div>
       </CardContent>
     </Card>
